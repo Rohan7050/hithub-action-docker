537a4a16a3ceb43a6647bf4cd1bdafb36a059d07
Rohan Pagare <rohanpagare7599@gmail.com>
2024-11-10 19:41:26 +0530

    initial commit

---
+++ routes/events.js
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+
+import db from '../data/database.js';
+
+const router = Router();
+
+router.get('/', async (req, res) => {
+  res.json({ events: [{id:1, name: 'event one'}] });
+});
+
+router.post('/', async (req, res) => {
+  const eventData = req.body;
+  res.status(201).json({
+    message: 'Event created.',
+    event: { ...eventData},
+  });
+});
+
+export default router;
