8b9149ef3802d6b27011053b470e3d2690df88d2
Rohan Pagare <rohanpagare7599@gmail.com>
2024-11-10 20:12:49 +0530

    add mongodb service container for testing

--- routes/events.js
+++ routes/events.js
@@ -1,18 +1,20 @@
 import { Router } from 'express';
 
-// import db from '../data/database.js';
+import db from '../data/database.js';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-  res.json({ events: [{id:1, name: 'event one'}] });
+  const allEvents = await db.collection('events').find().toArray();
+  res.json({ events: allEvents });
 });
 
 router.post('/', async (req, res) => {
   const eventData = req.body;
+  const result = await db.collection('events').insertOne({...eventData});
   res.status(201).json({
     message: 'Event created.',
-    event: { ...eventData, id: 1},
+    event: { ...eventData, id: result.insertedId },
   });
 });
 
